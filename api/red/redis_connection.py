6122ab6caf22faf3bc1dfb0a7ecf4adaa3ab52c6
Lucas <lcatolino@fi.uba.ar>
2022-06-30 17:48:39 -0300

    Add: redis endpoint connected to mongo if data is not found in cache

--- api/red/redis_connection.py
+++ api/red/redis_connection.py
@@ -1,8 +1,11 @@
 import sys
 from datetime import timedelta
 import json
+from fastapi.encoders import jsonable_encoder
 
 import redis
+
+from mongo.mongo_connection import retrieve_url_short
 
 #Connection
 def redis_connect() -> redis.client.Redis:
@@ -50,13 +53,14 @@ async def get_from_redis_cache(url: str) -> dict:
     
     else:
         # If cache is not found then sends request to Mongo Api
-        data = 'mongo' #await get_from_mongo(url) #TODO: si no esta en Redis, buscar en Mongo
+        data = await retrieve_url_short(url) #'mongo' #await get_from_mongo(url) #TODO: si no esta en Redis, buscar en Mongo
 
         # Saves the respose to redis and serves it directly
-        state = set_routes_to_cache(url, data)
+        returning_url= jsonable_encoder(data)['url_long']
+        state = set_routes_to_cache(key= url, value= returning_url)
 
         if state is True:
-            return json.dumps(data)
+            return returning_url
         return data
 
 
