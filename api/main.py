8d81ed2ee06f0778378c9a3fea0fed7d33458d36
Lucas <lcatolino@fi.uba.ar>
2022-07-02 13:38:28 -0300

    Add: CORS configuration (allows all)

--- api/main.py
+++ api/main.py
@@ -2,9 +2,24 @@ from fastapi import FastAPI
 from red.routes.urls import router as UrlRouter
 from mongo.routes.url import router as UrlRouterMongo
 from mongo.routes.access_log import router as AccessLogRouter
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
+
+#Cors origins
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "*"
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 #Redis endpoints
 app.include_router(UrlRouter, tags=["URL"], prefix="/url")
